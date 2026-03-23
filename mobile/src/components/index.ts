ebe9a35ace964d9cb663258f5c108f1cb404a7b0
StudyPro Dev <dev@studypro.app>
2026-03-23 15:45:59 -0300

    refactor: removendo mobile e focando 100% na versao web desktop

--- mobile/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { MissionCard } from './MissionCard';
-export { HealthPanel } from './HealthPanel';
-export { SessionForm } from './SessionForm';
-export { ReviewCard } from './ReviewCard';
--- mobile/src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { HomeScreen } from './HomeScreen';
-export { RegisterScreen } from './RegisterScreen';
-export { ReviewScreen } from './ReviewScreen';
-export { StatsScreen } from './StatsScreen';
-export { OnboardingScreen } from './OnboardingScreen';
-export { AuthScreen } from './AuthScreen';
